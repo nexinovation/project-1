3efc1e350e8286451b9796588eef2ed4177efe31
fearlessserver <senatoreniola@gmail.com>
2024-01-24 12:56:03 +0100

    added a javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+function temperature(){
+    var temp = document.getElementById("celcius").value;
+
+    // var temp = prompt("What is the temperature?");
+    // if (temp < 60){
+    //     alert("Wear a jacket");
+    // }
+    // else if (temp < 70){
+    //     alert("Wear a sweater");
+    // }
+    // else{
+    //     alert("Wear a t-shirt");
+    // }
+}
